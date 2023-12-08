e9c9ae74483abeeb769ea805fb5750b1d242ddec
Daniel Henrique <daniel.toledo@stone.com.br>
2023-12-08 14:24:05 -0300

    Update src/BreakEven.API/Strategies/Paycheck/UpdatePaycheckPayment.cs
    Co-authored-by: elfoosay <jsiilva@outlook.com.br>

--- src/BreakEven.API/Strategies/Paycheck/UpdatePaycheckPayment.cs
+++ src/BreakEven.API/Strategies/Paycheck/UpdatePaycheckPayment.cs
@@ -6,6 +6,7 @@ using BreakEven.API.Entities;
 public class UpdatePaycheckPayment : IUpdatePaycheckStrategy
 {
     public double Amount { get; init; }
+
     public  void UpdatePaycheck(Paycheck paycheck)
     {
         paycheck.Pay(Amount);
